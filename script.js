9434ab5d43e4c16827e9b3c09d27800353557f64
Mdshowanhossain <Mdshowanhossain11@gmail.com>
2021-07-06 12:25:48 +0600

    cruise-queen

--- script.js
+++ script.js
@@ -1,3 +1,41 @@
+function handleFirstClassTicket(isIncreased) {
+
+    const firstClassTicketPrice = document.getElementById("firstTicketInput");
+    const firstClassTicketPriceNumber = parseInt(firstClassTicketPrice.value);
+    // const newFirstClassTicket = firstClassTicketPriceNumber + 1;
+    let newFirstClassTicket = firstClassTicketPriceNumber;
+    if (isIncreased === true) {
+        newFirstClassTicket = firstClassTicketPriceNumber + 1;
+    }
+    else if (isIncreased === false && newFirstClassTicket > 0) {
+        newFirstClassTicket = firstClassTicketPriceNumber - 1;
+    }
+    firstClassTicketPrice.value = newFirstClassTicket;
+    const ticketPrice = newFirstClassTicket * 150;
+    document.getElementById("firstClassTicketPrice").innerText = 'First Class $' + ticketPrice;
+
+}
+
+function handleEconomyTicket(isIncreased) {
+    const economyTicketPrice = document.getElementById("economyTicketInput");
+    const economyTicketNumber = parseInt(economyTicketPrice.value);
+    // const newEconomyTicketNumber = economyTicketNumber + 1;
+    let newEconomyTicketNumber = economyTicketNumber;
+    if (isIncreased === true) {
+        newEconomyTicketNumber = economyTicketNumber + 1;
+    } else if (isIncreased === false && newEconomyTicketNumber > 0) {
+        newEconomyTicketNumber = economyTicketNumber - 1;
+    }
+    economyTicketPrice.value = newEconomyTicketNumber;
+    const ticketPrice = newEconomyTicketNumber * 100
+    document.getElementById("economyTicketPrice").innerText = 'Economy $' + ticketPrice
+
+}
+
+
+
+// First Method
+
 
 
 // document.getElementById("plusBtn").addEventListener("click", function () {
@@ -43,37 +81,3 @@
 // })
 
 
-
-function handleFirstClassTicket(isIncreased) {
-
-    const firstClassTicketPrice = document.getElementById("firstTicketInput");
-    const firstClassTicketPriceNumber = parseInt(firstClassTicketPrice.value);
-    // const newFirstClassTicket = firstClassTicketPriceNumber + 1;
-    let newFirstClassTicket = firstClassTicketPriceNumber;
-    if (isIncreased === true) {
-        newFirstClassTicket = firstClassTicketPriceNumber + 1;
-    }
-    else if (isIncreased === false && newFirstClassTicket > 0) {
-        newFirstClassTicket = firstClassTicketPriceNumber - 1;
-    }
-    firstClassTicketPrice.value = newFirstClassTicket;
-    const ticketPrice = newFirstClassTicket * 150;
-    document.getElementById("firstClassTicketPrice").innerText = 'First Class $' + ticketPrice;
-
-}
-
-function handleEconomyTicket(isIncreased) {
-    const economyTicketPrice = document.getElementById("economyTicketInput");
-    const economyTicketNumber = parseInt(economyTicketPrice.value);
-    // const newEconomyTicketNumber = economyTicketNumber + 1;
-    let newEconomyTicketNumber = economyTicketNumber;
-    if (isIncreased === true) {
-        newEconomyTicketNumber = economyTicketNumber + 1;
-    } else if (isIncreased === false && newEconomyTicketNumber > 0) {
-        newEconomyTicketNumber = economyTicketNumber - 1;
-    }
-    economyTicketPrice.value = newEconomyTicketNumber;
-    const ticketPrice = newEconomyTicketNumber * 100
-    document.getElementById("economyTicketPrice").innerText = 'Economy $' + ticketPrice
-
-}
